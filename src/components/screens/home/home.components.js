73c2bf8bc6b2f9bbe7b58f69f99eb4401cb2727e
Marina Obruch <marina_obruch@mail.ru>
2023-11-21 22:15:42 +0500

    Custom-query | create base functional

--- src/components/screens/home/home.components.js
+++ src/components/screens/home/home.components.js
@@ -1,5 +1,6 @@
 import { BaseScreen } from "@/core/component/base-screen.component";
 import renderService from "@/core/services/render.service";
+import { $R } from "@/core/query/query.lib";
 
 import styles from './home.module.scss'
 import template from "./home.template.html"
@@ -10,6 +11,9 @@ export class Home extends BaseScreen {
     }
     render() {
 		const element = renderService.htmlToElement(template, [], styles)
+
+        $R(element).find("h1").css("color", "green")
+
 		return element.outerHTML
     }
 }
