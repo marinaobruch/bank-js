a380b6baa1c60e6d3b41187495732f3837aa7893
Marina Obruch <marina_obruch@mail.ru>
2023-11-22 21:36:38 +0500

    Components | UI-filed

--- src/components/screens/home/home.components.js
+++ src/components/screens/home/home.components.js
@@ -5,20 +5,14 @@ import { $R } from "@/core/query/query.lib";
 import styles from './home.module.scss'
 import template from "./home.template.html"
 import { Button } from "@/components/ui/button/button.component";
+import { Field } from "@/components/ui/field/field.component";
 export class Home extends BaseScreen {
     constructor() {
         // наследование - используем функцию super, чтобы прокинуть аргумент вовнутрь BaseScreen
         super({ title: "Home" })
     }
     render() {
-		const element = renderService.htmlToElement(
-            template,
-            [
-                new Button({
-                    children: "Send",
-                    onClick: () => alert("Hey"),
-                    variant: "green"
-        })], styles)
+		const element = renderService.htmlToElement(template,[], styles)
 
         $R(element).find("h1").css("color", "green")
 
--- src/core/query/query.lib.js
+++ src/core/query/query.lib.js
@@ -1,6 +1,8 @@
 // $R('#rrt').find('.rge').css('', '').text('').html('')
 // пример, как будет выглядеть запрос извне: $R(element).find("h1").css("color", "green")
 
+import { formatCardNumberWithDashes } from "@/utils/format/format-card-number"
+
 /**
  * Represents the RQuery class for working with DOM elements.
  */
@@ -26,6 +28,7 @@ class RQuery {
 		}
 	}
 
+	/* FIND */
 
 	/**
 	 * Find the first element that matches the specified selector within the selected element.
@@ -43,6 +46,8 @@ class RQuery {
 			throw new Error(`Element ${selector} not found!`)
 		}
 	}
+
+		/* INSERT */
 
     /**
 	 * Append a new element as a child of the selected element.
@@ -89,6 +94,8 @@ class RQuery {
 		}
 	}
 
+		/* EVENTS */
+
 	/**
 	 * Attach a click event listener to the selected element.
 	 * @param {function(Event): void} callback - The event listener function to execute when the selected element is clicked. The function will receive the event object as its argument.
@@ -98,6 +105,76 @@ class RQuery {
 			this.element.addEventListener('click', callback)
 			return this
 		}
+
+		/* FORM */
+
+	/**
+	 * Set attributes and event listeners for an input element.
+	 * @param {object} options - An object containing input options.
+	 * @param {function(Event): void} [options.onInput] - The event listener for the input's input event.
+	 * @param {object} [options.rest] - Optional attributes to set on the input element.
+	 * @returns {RQuery} The current RQuery instance for chaining.
+	 */
+	// получаем все опции для поля ввода, в т.ч. получаем отдельно onInput, чтобы повешать на него слушатель
+	input({ onInput, ...rest }) {
+		if (this.element.tagName.toLowerCase() !== 'input')
+			throw new Error('Element must be an input')
+
+		for (const [key, value] of Object.entries(rest)) {
+			this.element.setAttribute(key, value)
+		}
+
+		if (onInput) {
+			this.element.addEventListener('input', onInput)
+		}
+
+		return this
+	}
+
+	/**
+	 * Set attributes and event listeners for a number input element.
+	 * @param {number} [limit] - The maximum length of input value.
+	 * @returns {RQuery} The current RQuery instance for chaining.
+	 */
+	numberInput(limit) {
+		if (
+			this.element.tagName.toLowerCase() !== 'input' || this.element.type !== 'number'
+		)
+			throw new Error('Element must be an input with type "number"')
+
+		this.element.addEventListener('input', event => {
+			// event.target.value - получаем текущее значение из поля, в которое что-то ввели,
+			// далее все цифры 0-9 (g - глобально) заменяем на пустоту, те не должно быть НЕ чисел
+			let value = event.target.value.replace(/[^0-9]/g, '')
+			if (limit) value = value.substring(0, limit)
+			event.target.value = value
+		})
+
+		return this
+	}
+
+	/**
+	 * Set attributes and event listeners for a credit card input element.
+	 * @returns {RQuery} The current RQuery instance for chaining.
+	 */
+	creditCardInput() {
+		const limit = 16
+
+		if (
+			this.element.tagName.toLowerCase() !== 'input' || this.element.type !== 'text'
+		)
+			throw new Error('Element must be an input with type "text"')
+
+		this.element.addEventListener('input', event => {
+			let value = event.target.value.replace(/[^0-9]/g, '')
+			if (limit) value = value.substring(0, limit)
+			event.target.value = formatCardNumberWithDashes(value)
+		})
+
+		return this
+	}
+
+			/* STYLES */
 
 	/**
 	 * Set the CSS style of the selected element.
